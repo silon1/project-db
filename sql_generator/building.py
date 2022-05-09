a791a47988ebdf6fe227deb2865bafdbbb723366
a <6g6A07tAE@protonmail.com>
2022-05-09 19:23:04 +0300

    buildings and rooms tables

--- sql_generator/building.py
+++ sql_generator/building.py
@@ -1,21 +1,27 @@
-from .csv_writer import writer
+from csv_writer import writer
 from random import randint as rnd
 from faker import Faker
+from time import time as t
 
 dep_id_range = [0, 1001+1]
 building_id_range = [1, 20001]
 
-file = 'building.csv'
+file = 'buildings.csv'
 fields = ['building_id', 'building_name', 'department_id']
 rows = []
 
 names = set()
 fake = Faker()
 
-for i in range(*building_id_range):
-    while (name := fake.name()) in names:
-        pass
-    names.add(name)
-    rows += [i, name, rnd(*dep_id_range)]
+if __name__ == '__main__':
+    t1 = t()
 
-writer(file, fields, rows)
+    for i in range(*building_id_range):
+        while (name := fake.name()) in names:
+            pass
+        names.add(name)
+        rows += [[i, name, rnd(*dep_id_range)]]
+
+    t2 = t()
+    writer(file, fields, rows)
+    print(t2 - t1, t() - t2, sep='\n')
--- sql_generator/room.py
+++ sql_generator/room.py
@@ -1,24 +1,33 @@
-import random
-from .csv_writer import writer
-from .building import building_id_range
+from csv_writer import *
+from building import building_id_range
 from random import randint as rnd, choice
+import multiprocessing
+from time import time as t
+from copy import deepcopy as dcp
 
 dep_id_range = [0, 1001+1]
 wing_id_range = [0, 399+1]
 
-room_type = ['show', 'services', 'workshow', 'administration']
+room_type = ['show']*10 + ['services']*2 + ['workshow'] + ['administration']
 base_rooms = ['lobby', 'services', 'show', 'show', 'show', 'show']
 
-file = 'building.csv'
+file = 'rooms.csv'
 fields = ['room_id', 'room_type', 'wing_id', 'building_id']
-rows = []
 
 names = set()
 
-for i in range(*building_id_range):
-    rooms = base_rooms
+def generate_rooms(i):
+    rooms = dcp(base_rooms)
     rooms += [choice(room_type) for _ in range(rnd(2, 5))]
-    for j, room in enumerate(rooms):
-        rows += [j, room, choice(range(*wing_id_range)), i]
+    return [[f'{i}{j+1}', room, choice(range(*wing_id_range)), i] for j, room in enumerate(rooms)]
 
-writer(file, fields, rows)
+if __name__ == '__main__':
+    t1 = t()
+    rows = []
+    with multiprocessing.Pool(4) as p:
+        for r in p.map(generate_rooms, list(range(*building_id_range))):
+            rows += r
+    t2 = t()
+    writer(file, fields, rows)
+    print(t2 - t1, t() - t1, sep='\n')
+
