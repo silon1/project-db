08bbc0cdc967f99665d42646240afa012e47b9ec
aviheller7 <46092027+aviheller7@users.noreply.github.com>
2022-05-19 20:16:19 +0300

    Update room.py
    from wing_id as a foreign key
    to department_id as a foreign key

--- sql_generator/room.py
+++ sql_generator/room.py
@@ -6,20 +6,20 @@ from time import time as t
 from copy import deepcopy as dcp
 
 dep_id_range = [0, 1001+1]
-wing_id_range = [0, 399+1]
+department_id_range = [0, 399+1]
 
 room_type = [1]*10 + [2]*2 + [4] + [5]
 base_rooms = [3, 2, 1, 1, 1, 1]
 
 file = 'rooms.csv'
-fields = ['room_id', 'room_type_id', 'wing_id', 'building_id']
+fields = ['room_id', 'room_type_id', 'department_id', 'building_id']
 
 names = set()
 
 def generate_rooms(i):
     rooms = dcp(base_rooms)
     rooms += [choice(room_type) for _ in range(rnd(2, 5))]
-    return [[f'{i}{j+1}', room, choice(range(*wing_id_range)), i] for j, room in enumerate(rooms)]
+    return [[f'{i}{j+1}', room, choice(range(*department_id_range)), i] for j, room in enumerate(rooms)]
 
 if __name__ == '__main__':
     t1 = t()
