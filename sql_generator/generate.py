dec11145b66d18334661af89b133f79c62d2f5fa
a <6g6A07tAE@protonmail.com>
2022-05-23 16:21:49 +0300

    .

--- sql_generator/generate.py
+++ sql_generator/generate.py
@@ -3,4 +3,4 @@ import subprocess
 subprocess.call("building.py", shell=True)
 subprocess.call("room_type.py", shell=True)
 subprocess.call("room.py", shell=True)
-subprocess.call("equipments.py", shell=True)
+#subprocess.call("equipments.py", shell=True)
