ac7ab1b11a68f55da2e31320c71f272c74358fa6
a <6g6A07tAE@protonmail.com>
2022-05-23 17:58:40 +0300

    .

--- sql_generator/set_eq.py
+++ sql_generator/set_eq.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
-    f = set(open('equipments.txt', 'r').read().replace(' & ', '\n').splitlines())
+    f = open('equipments.txt', 'r').read().lower().replace(' & ', '\n').splitlines()
+    print(len(f), len(f:=(set(f))))
+    for i in f:
+        if len(i) > 40:
+            print(f'ERROR: {i}')    
     open('equipments.txt', 'w').write('\n'.join(f))
-    print(len(f))
