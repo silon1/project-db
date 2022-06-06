dd19229393dbf5e52eb6dd9f3d122895ec07f285
a <6g6A07tAE@protonmail.com>
2022-06-06 21:53:09 +0300

    order files

--- sql_generator/csv_writer.py
+++ sql_generator/csv_writer.py
@@ -1,7 +1,7 @@
 import csv
 
 def writer(file, fields, rows):
-    with open(file, 'w') as f:
+    with open('../tabels/' + file, 'w') as f:
         a = csv.writer(f)
         a.writerow(fields)
         a.writerows(rows)
