fbcf02c78697b6a44b7071c265f50a2df0e3b7f5
silon1 <73113245+silon1@users.noreply.github.com>
2022-06-12 23:59:59 +0300

    Update most_founder_per_wing.sql

--- queries/most_founder_per_wing.sql
+++ queries/most_founder_per_wing.sql
@@ -1,4 +1,8 @@
 SELECT b.wing_id, b.building_last_name, COUNT(*) c
-FROM Buildings b
+FROM aheller.Buildings b
 GROUP BY b.wing_id, b.building_last_name
+HAVING COUNT(b.building_last_name) = (SELECT MAX(COUNT(building_last_name))
+                                      FROM aheller.buildings
+                                      WHERE wing_id = b.wing_id
+                                      GROUP BY building_last_name)
 ORDER BY b.wing_id, c DESC
