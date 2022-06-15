1b1ad638fe4bda39eaf173cc6e0011cf5d6870b3
aviheller7 <46092027+aviheller7@users.noreply.github.com>
2022-06-15 16:09:06 +0300

    Update to_order.sql

--- queries/to_order.sql
+++ queries/to_order.sql
@@ -1,2 +1,8 @@
+/*
+-- before index
+SELECT e.equipment_name, e.building_id, e.amount, e.min_amount, e.min_amount - e.amount as to_order
+FROM Equipments e
+WHERE e.amount < e.min_amount*/
+
 SELECT *
 FROM TO_ORDER
