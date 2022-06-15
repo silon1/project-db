650b379730cc883a9f0eb288b9635e4e2637e9a0
aviheller7 <46092027+aviheller7@users.noreply.github.com>
2022-06-15 16:22:25 +0300

    Update goob_buildings.sql

--- queries/goob_buildings.sql
+++ queries/goob_buildings.sql
@@ -1,4 +1,14 @@
-SELECT b.building_id
-FROM Building b
+/*
+-- before index
+SELECT b.building_id, b.building_first_name||' '||b.building_last_name as name
+FROM buildings b
+WHERE b.building_id NOT IN (SELECT e.building_id
+                            FROM Equipments e
+                            WHERE e.amount < e.min_amount)
+*/
+
+
+SELECT b.building_id, b.building_first_name||' '||b.building_last_name as name
+FROM buildings b
 WHERE b.building_id NOT IN (SELECT t.building_id
                             FROM TO_ORDER t)
